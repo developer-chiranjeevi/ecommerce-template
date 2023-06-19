a5e36b029198a9de287a8978c0f5a4fabf98ea68
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-06-19 19:15:37 +0530

    status : V5

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,8 +11,8 @@ const Products = () =>{
             <div className="">
                 <ul className="capitalize inline-flex">
                     <li className="text-sm lg:text-md mx-2 lg:mx-4 bg-gray-900 text-white border-2 border-gray-800 px-4 py-1 rounded-full"><a href="#" className="">electronics</a></li>
-                    <li className="text-sm lg:text-md mx-4 border-2 border-gray-800 px-4 py-1 rounded-full"><a href="#">furnitures</a></li>
-                    <li className="text-sm lg:text-md mx-4 border-2 border-gray-800 px-4 py-1 rounded-full"><a href="#">clothing</a></li>
+                    <li className="text-sm lg:text-md mx-2 border-2 border-gray-800 px-4 py-1 rounded-full"><a href="#">furnitures</a></li>
+                    <li className="text-sm lg:text-md mx-2 border-2 border-gray-800 px-4 py-1 rounded-full"><a href="#">clothing</a></li>
                 </ul>
             </div>
             <div className="w-full flex flex-col md:flex-row flex-wrap justify-between">
