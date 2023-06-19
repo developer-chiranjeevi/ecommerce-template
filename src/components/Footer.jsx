067981aa56033053b748d4a9bdc020c7b9d36c4a
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-06-19 19:29:05 +0530

    status : V5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () =>{
                 <h1 className="capitalize text-md text-center lg:text-xl lg:text-start">sign up for our newsletter to get latest notifications on our products</h1>
                 <div className="flex">
                     <input className="focus:outline-none mx-auto px-2 py-3 w-3/5 rounded-lg mt-2" type="email" placeholder="Enter Your Email Address" />
-                    <button className="bg-red-600 text-white py-3 px-8 rounded-r-lg relative right-1 lg:px-5">Subscribe</button>           
+                    <button className="bg-blue-600 text-white py-2 px-8 rounded-r-lg relative right-1 lg:px-5">Subscribe</button>           
                 </div>
             </div>
             <div className="w-full hidden mt-5 lg:mt-0 lg:w-1/2  lg:flex lg:justify-end">
